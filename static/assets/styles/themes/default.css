b19cc7b7f1207b46a1d51bafae309b3706590e10
UltraN0va288 <125221897+UltraN0va288@users.noreply.github.com>
2024-04-15 14:00:37 -0500

    Add files via upload

---
+++ static/assets/styles/themes/default.css
@@ -0,0 +1,23 @@
+:root {
+  --page: url('/./assets/media/background/full2.png');
+  --background: rgba(255, 255, 255, 0.02);
+  --background-filter: blur(10px) brightness(80%);
+  --background-secondary: #4545459e;
+  --background-dark: #121212;
+  --background-other: #282828;
+
+  --title: #fff;
+  --block: #2a2a2a;
+  --block-buttons: #333;
+  --input: #444;
+  --column: #353535;
+  --accent-color: #444;
+  --main: #222;
+
+  --text-primary: #fff;
+  --text-secondary: #b0b0b0;
+  --text-placeholder: #aaa;
+  --text-dark: #555;
+
+  --transparent: rgb(0, 0, 0, 0);
+}
