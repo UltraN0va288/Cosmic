23f5f7e1159f3d45ed58e74c63de01cd9e8aa6c4
UltraN0va288 <125221897+UltraN0va288@users.noreply.github.com>
2024-04-21 15:05:58 -0500

    Update main.js

--- static/assets/scripts/main.js
+++ static/assets/scripts/main.js
@@ -165,3 +165,30 @@ document.addEventListener('DOMContentLoaded', function () {
     document.body.style.backgroundImage = "url('" + savedBackgroundImage + "')"
   }
 })
+
+const trailer = document.getElementById('trailer')
+const trailer2 = document.getElementById('trailer2')
+const trailer3 = document.getElementById('trailer3')
+const trailer4 = document.getElementById('trailer4')
+
+window.onmousemove = e => {
+  const x = e.clientX - trailer.offsetWidth / 2,
+        y = e.clientY - trailer.offsetHeight / 2;
+  
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px)`
+  }
+  
+  trailer.animate(keyframes, {
+    duration: 800
+  })
+  trailer2.animate(keyframes, {
+    duration: 1200
+  })
+  trailer3.animate(keyframes, {
+    duration: 1600
+  })
+  trailer4.animate(keyframes, {
+    duration: 2000
+  })
+}
