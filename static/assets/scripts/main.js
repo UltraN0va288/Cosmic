133d28370fecb71a834379d71862198f38842a3f
UltraN0va288 <125221897+UltraN0va288@users.noreply.github.com>
2024-05-16 10:27:56 -0500

    Update main.js

--- static/assets/scripts/main.js
+++ static/assets/scripts/main.js
@@ -1,22 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Ads
-  if (localStorage.getItem('ad') === null || localStorage.getItem('ad') === 'default') {
-    localStorage.setItem('ad', 'on')
-  }
-
-  var advDiv = document.getElementById('adv')
-  if (advDiv && localStorage.getItem('ad') === 'default') {
-    var script = document.createElement('script')
-    script.type = 'text/javascript'
-    script.src = '//oysterscoldtiny.com/1c/c3/8a/1cc38a6899fdf8ba4dfe779bcc54627b.js'
-    advDiv.appendChild(script)
-    console.log('Script inserted inside the adv div.')
-  } else if (advDiv && localStorage.getItem('ad') === 'off') {
-    advDiv.remove()
-    console.log('The adv div has been removed.')
-  }
-})
-
 window.localStorage.setItem('v4Particles', 'true')
 
 // Nav
@@ -166,29 +147,157 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 })
 
-const trailer = document.getElementById('trailer')
-const trailer2 = document.getElementById('trailer2')
-const trailer3 = document.getElementById('trailer3')
-const trailer4 = document.getElementById('trailer4')
 
-window.onmousemove = e => {
-  const x = e.clientX - trailer.offsetWidth / 2,
-        y = e.clientY - trailer.offsetHeight / 2;
+//trailer
+let start = new Date().getTime();
+
+const originPosition = { x: 0, y: 0 };
+
+const last = {
+  starTimestamp: start,
+  starPosition: originPosition,
+  mousePosition: originPosition
+}
+
+const config = {
+  starAnimationDuration: 1500,
+  minimumTimeBetweenStars: 250,
+  minimumDistanceBetweenStars: 75,
+  glowDuration: 500,
+  maximumGlowPointSpacing: 10,
+  colors: ["249 146 253", "252 254 255"],
+  sizes: ["1.4rem", "1rem", "0.6rem"],
+  animations: ["fall-1", "fall-2", "fall-3"]
+}
+
+let count = 0;
   
-  const keyframes = {
-    transform: `translate(${x}px, ${y}px)`
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
+      selectRandom = items => items[rand(0, items.length - 1)];
+
+const withUnit = (value, unit) => `${value}${unit}`,
+      px = value => withUnit(value, "px"),
+      ms = value => withUnit(value, "ms");
+
+const calcDistance = (a, b) => {
+  const diffX = b.x - a.x,
+        diffY = b.y - a.y;
+  
+  return Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+}
+
+const calcElapsedTime = (start, end) => end - start;
+
+const appendElement = element => document.body.appendChild(element),
+      removeElement = (element, delay) => setTimeout(() => document.body.removeChild(element), delay);
+
+const createStar = position => {
+  const star = document.createElement("span"),
+        color = selectRandom(config.colors);
+  
+  star.className = "star fa-solid fa-sparkle";
+  
+  star.style.left = px(position.x);
+  star.style.top = px(position.y);
+  star.style.fontSize = selectRandom(config.sizes);
+  star.style.color = `rgb(${color})`;
+  star.style.textShadow = `0px 0px 1.5rem rgb(${color} / 0.5)`;
+  star.style.animationName = config.animations[count++ % 3];
+  star.style.starAnimationDuration = ms(config.starAnimationDuration);
+  
+  appendElement(star);
+
+  removeElement(star, config.starAnimationDuration);
+}
+
+const createGlowPoint = position => {
+  const glow = document.createElement("div");
+  
+  glow.className = "glow-point";
+  
+  glow.style.left = px(position.x);
+  glow.style.top = px(position.y);
+  
+  appendElement(glow)
+  
+  removeElement(glow, config.glowDuration);
+}
+
+const determinePointQuantity = distance => Math.max(
+  Math.floor(distance / config.maximumGlowPointSpacing),
+  1
+);
+
+/* --  
+
+The following is an explanation for the "createGlow" function below:
+
+I didn't cover this in my video, but I ran into an issue where moving the mouse really quickly caused gaps in the glow effect. Kind of like this:
+
+*   *       *       *    *      *    🖱️
+
+instead of:
+
+*************************************🖱️
+
+To solve this I sort of "backfilled" some additional glow points by evenly spacing them in between the current point and the last one. I found this approach to be more visually pleasing than one glow point spanning the whole gap.
+
+The "quantity" of points is based on the config property "maximumGlowPointSpacing".
+
+My best explanation for why this is happening is due to the mousemove event only firing every so often. I also don't think this fix was totally necessary, but it annoyed me that it was happening so I took on the challenge of trying to fix it.
+
+-- */
+const createGlow = (last, current) => {
+  const distance = calcDistance(last, current),
+        quantity = determinePointQuantity(distance);
+  
+  const dx = (current.x - last.x) / quantity,
+        dy = (current.y - last.y) / quantity;
+  
+  Array.from(Array(quantity)).forEach((_, index) => { 
+    const x = last.x + dx * index, 
+          y = last.y + dy * index;
+    
+    createGlowPoint({ x, y });
+  });
+}
+
+const updateLastStar = position => {
+  last.starTimestamp = new Date().getTime();
+
+  last.starPosition = position;
+}
+
+const updateLastMousePosition = position => last.mousePosition = position;
+
+const adjustLastMousePosition = position => {
+  if(last.mousePosition.x === 0 && last.mousePosition.y === 0) {
+    last.mousePosition = position;
+  }
+};
+
+const handleOnMove = e => {
+  const mousePosition = { x: e.clientX, y: e.clientY }
+  
+  adjustLastMousePosition(mousePosition);
+  
+  const now = new Date().getTime(),
+        hasMovedFarEnough = calcDistance(last.starPosition, mousePosition) >= config.minimumDistanceBetweenStars,
+        hasBeenLongEnough = calcElapsedTime(last.starTimestamp, now) > config.minimumTimeBetweenStars;
+  
+  if(hasMovedFarEnough || hasBeenLongEnough) {
+    createStar(mousePosition);
+    
+    updateLastStar(mousePosition);
   }
   
-  trailer.animate(keyframes, {
-    duration: 800
-  })
-  trailer2.animate(keyframes, {
-    duration: 1200
-  })
-  trailer3.animate(keyframes, {
-    duration: 1600
-  })
-  trailer4.animate(keyframes, {
-    duration: 2000
-  })
+  createGlow(last.mousePosition, mousePosition);
+  
+  updateLastMousePosition(mousePosition);
 }
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
+
+document.body.onmouseleave = () => updateLastMousePosition(originPosition);
